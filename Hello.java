04ad32e59ea5ca7981770bb74280264547f7b3c5
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-10-02 20:48:30 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+	
+public class Hello
+{ 
+    public static void main(String args[]) 
+    { 
+	    	System.out.println("Hello");
+    } 
+} 
+
+ 
+ 
+ 
