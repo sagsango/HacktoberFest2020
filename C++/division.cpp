093814f24a47914b3702e726f91cc478b0ced63e
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-10-02 22:30:58 +0530

    Update division.cpp

--- C++/division.cpp
+++ C++/division.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  int a,b; // Note b != 0
+  double a,b; // Note b != 0
   cin >> a >> b;
   cout << a <<" / " << b <<" = " << a / b << endl;
 }
