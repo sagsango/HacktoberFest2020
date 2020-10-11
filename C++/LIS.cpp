30b0d9b74e8f5bc21b7e3fe06fff27f6975d9f16
Sagar Singh <34793802+sagsango@users.noreply.github.com>
2020-10-11 11:37:25 +0530

    Update LIS.cpp

--- C++/LIS.cpp
+++ C++/LIS.cpp
@@ -1,8 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Not strictly increasing
+int nax = 1e5;
+int BIT[nax+1];
+void update(int i,int val){
+  while( i <= nax ){
+      BIT[i] = max(BIT[i],val);
+      i += i & -i;
+  }
+}
+int query(int i,int val=0){
+  while( i > 0 ){
+    val = max(val,BIT[i]);
+    i -= i & -i;
+  }
+  return val;
+}
 int main(){
-int n;cin>>n;
-int a[n];for(int i=0;i<n;i++)cin>>a[i];
+  int n;cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++)
+     cin>>a[i]; // all values from 1 to 1e5
+  int mxl = 1;
+  for(int i=0;i<n;i++){
+      inr tmp = query(a[i]);
+      update(a[i],tmp+1);
+      mxl = max(mxl,tmp+1);
+  }
+  cout << "Maximum Inceasing Subsequence [ Non Strict ] : " << mxl << endl;
+  
 
 }
